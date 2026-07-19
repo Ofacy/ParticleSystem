d2f20210933877169a1991b19a23965368d91877
Ofacy <63876319+Ofacy@users.noreply.github.com>
2026-07-19 04:08:21 +0200

    chore: remove unused functions from egui_renderer

--- src/egui_renderer.rs
+++ src/egui_renderer.rs
@@ -8,7 +8,6 @@ use winit::window::Window;
 pub struct EguiRenderer {
     state: State,
     renderer: Renderer,
-    frame_started: bool,
 }
 
 impl EguiRenderer {
@@ -44,16 +43,11 @@ impl EguiRenderer {
         EguiRenderer {
             state: egui_state,
             renderer: egui_renderer,
-            frame_started: false,
         }
     }
 
     pub fn handle_input(&mut self, window: &Window, event: &WindowEvent) -> EventResponse {
         return self.state.on_window_event(window, event);
-    }
-
-    pub fn ppp(&mut self, v: f32) {
-        self.context().set_pixels_per_point(v);
     }
     
     pub fn run_ui(
@@ -105,65 +99,4 @@ impl EguiRenderer {
         }
     }
 
-    pub fn begin_frame(&mut self, window: &Window) {
-        let raw_input = self.state.take_egui_input(window);
-        self.state.egui_ctx().begin_pass(raw_input);
-        self.frame_started = true;
-    }
-
-    pub fn end_frame_and_draw(
-        &mut self,
-        device: &Device,
-        queue: &Queue,
-        encoder: &mut CommandEncoder,
-        window: &Window,
-        window_surface_view: &TextureView,
-        screen_descriptor: ScreenDescriptor,
-    ) {
-        if !self.frame_started {
-            panic!("begin_frame must be called before end_frame_and_draw can be called!");
-        }
-
-        self.ppp(screen_descriptor.pixels_per_point);
-
-        let full_output = self.state.egui_ctx().end_pass();
-
-        self.state
-            .handle_platform_output(window, full_output.platform_output);
-
-        let tris = self
-            .state
-            .egui_ctx()
-            .tessellate(full_output.shapes, self.state.egui_ctx().pixels_per_point());
-        for (id, image_delta) in &full_output.textures_delta.set {
-            self.renderer
-                .update_texture(device, queue, *id, image_delta);
-        }
-        self.renderer
-            .update_buffers(device, queue, encoder, &tris, &screen_descriptor);
-        let rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            multiview_mask: None,
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                depth_slice: None,
-                view: window_surface_view,
-                resolve_target: None,
-                ops: egui_wgpu::wgpu::Operations {
-                    load: egui_wgpu::wgpu::LoadOp::Load,
-                    store: StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            label: Some("egui main render pass"),
-            occlusion_query_set: None,
-        });
-
-        self.renderer
-            .render(&mut rpass.forget_lifetime(), &tris, &screen_descriptor);
-        for x in &full_output.textures_delta.free {
-            self.renderer.free_texture(x)
-        }
-
-        self.frame_started = false;
-    }
 }
